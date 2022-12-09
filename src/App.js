6969722d6515e5b4f5307273052d1cb4434beec7
Abdullahkhaliq932 <abdullahkhaliq321@gmail.com>
2022-12-09 16:16:21 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+
+import Header from './components/header/Header'
+import Products from './components/products/Products'
+import Cart from './components/cart/Cart'
+
+function App() {
+  return (
+    <div className="container py-3">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
